3738c2775fc3ce9623ca2b8c62ed5603191a52e1
PSIvanov22 <PSIvanov22@codingburgas.bg>
2025-06-29 20:42:22 +0300

    Add SeatManager.cpp and SeatManager.h

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Админ/Documents/movie-ticket-booking-system-10th-grade-PSIvanov22
+# Install script for directory: C:/Users/dimit/OneDrive/Работен плот/movie-ticket-booking-system-10th-grade-PSIvanov22
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Админ/Documents/movie-ticket-booking-system-10th-grade-PSIvanov22/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/dimit/OneDrive/Работен плот/movie-ticket-booking-system-10th-grade-PSIvanov22/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,13 +32,25 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Users/dimit/OneDrive/Работен плот/movie-ticket-booking-system-10th-grade-PSIvanov22/out/build/x64-Debug/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
 if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
+    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+  else()
+    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
+    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
+    unset(CMAKE_INST_COMP_HASH)
+  endif()
 else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Админ/Documents/movie-ticket-booking-system-10th-grade-PSIvanov22/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Users/dimit/OneDrive/Работен плот/movie-ticket-booking-system-10th-grade-PSIvanov22/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
