cf4fcfd482b462296246de0514323c097d930539
PSIvanov22 <PSIvanov22@codingburgas.bg>
2025-06-29 20:51:41 +0300

    Add the Essentials in SeatManager.h

--- SeatManager.h
+++ SeatManager.h
@@ -1,1 +1,23 @@
 #pragma once
+#ifndef SEAT_MANAGER_H
+#define SEAT_MANAGER_H
+
+#define ROWS 15
+#define COLS 40
+#define MOVIE_COUNT 8
+
+extern int vipCount[MOVIE_COUNT];
+extern int premiumCount[MOVIE_COUNT];
+extern int commonCount[MOVIE_COUNT];
+extern int totalRevenue[MOVIE_COUNT];
+
+extern const char* movieTitles[MOVIE_COUNT];
+extern const char* movieTimes[MOVIE_COUNT];
+
+void clearScreen();
+void printLogo();
+void displaySeatingMap(int** seats);
+void reserveSeat(int*** allSeats, int movieIndex);
+void showRevenue(int movieIndex);
+
+#endif
